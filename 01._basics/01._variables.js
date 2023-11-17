4d740f8c8277b74db2b1dbf6313aba9e52a51d8c
krishnakanth699 <71022371+krishnakanth699@users.noreply.github.com>
2023-11-17 12:59:10 +0000

    Learnt about variables in javascript

---
+++ 01._basics/01._variables.js
@@ -0,0 +1,15 @@
+
+const accountId = 1234
+let accountUserName = 'krishnakanth699'
+var accountName = 'Krishnakanth D'
+
+
+accountUserName = 'krishnakanth69'
+
+/*
+
+Prefer Not to use var because of the block scope and functional scope
+
+*/
+
+console.table([accountId,accountUserName,accountName]);
