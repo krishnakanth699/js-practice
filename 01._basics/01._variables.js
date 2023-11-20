4eea9aee421e8e416153c2d3c6e6d7d8ef92573d
krishnakanth699 <71022371+krishnakanth699@users.noreply.github.com>
2023-11-20 12:14:41 +0000

    datatypes learnt

--- 01._basics/01._variables.js
+++ 01._basics/01._variables.js
@@ -2,6 +2,7 @@
 const accountId = 1234
 let accountUserName = 'krishnakanth699'
 var accountName = 'Krishnakanth D'
+let accountState;
 
 
 accountUserName = 'krishnakanth69'
@@ -12,4 +13,4 @@ Prefer Not to use var because of the block scope and functional scope
 
 */
 
-console.table([accountId,accountUserName,accountName]);
+console.table([accountId,accountUserName,accountName,accountState]);
---
+++ 01._basics/02._data_types.js
@@ -0,0 +1,24 @@
+'use strict'; // treat all js code as newer version
+
+// alert("yoyo") // Cant be used in vs code because this is not the browser
+
+let name = "Hello"
+let age = 18
+let isLoggedIn = true
+let state
+
+// number -> 2 to power 53
+// bigint
+// string -> ""
+// boolean
+// null => standalone value
+// undefined
+// symbol -> mostly used in symbol
+
+
+// object
+
+console.log(typeof state)
+console.log(typeof null) // type is object
+console.log(typeof undefined) // undefined
+
