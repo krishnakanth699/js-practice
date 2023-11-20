f89950d4ec7bad25b8e4fae0e440491a4eebea97
krishnakanth699 <71022371+krishnakanth699@users.noreply.github.com>
2023-11-20 12:44:24 +0000

    data type conversion learnt

---
+++ 01._basics/03._conversion_operations.js
@@ -0,0 +1,15 @@
+'use strict';
+
+// let count = "22"
+// let count = "22ab"
+let count = "-1"
+
+let convert = Number(count)
+
+console.log(convert)
+console.log(typeof(convert))
+
+let stringNumber = String("22")
+
+console.log(typeof(stringNumber))
+
