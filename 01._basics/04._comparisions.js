7aed99cc7880165a3c3b20c618e0d5f6871ad208
krishnakanth699 <71022371+krishnakanth699@users.noreply.github.com>
2023-11-20 13:39:29 +0000

    Comparisions between datatype learnt

---
+++ 01._basics/04._comparisions.js
@@ -0,0 +1,18 @@
+'use strict';
+
+console.log(null > 0)
+console.log(null == 0)
+console.log(null >= 0 )
+
+console.log(' ')
+
+console.log(undefined > 0)
+console.log(undefined == 0)
+console.log(undefined >= 0)
+
+/*
+
+Try to avoid comparisions of different datatype while comparing - It is one of the flaw in
+Javascript
+
+*/
